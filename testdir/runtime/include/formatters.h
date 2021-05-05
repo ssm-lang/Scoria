d89a9b8e42db57b95a01279fc06a4c8f346f4bc9
Robert Krook <krookr@chalmers.se>
2021-05-05 14:55:12 +0200

    did some cleanup

--- testdir/runtime/include/formatters.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PENG_FORMATTERS_H
-#define _PENG_FORMATTERS_H
-#endif
-
-#define format_int "%d"
-#define format_bool "%d"
--- testdir/runtime/include/peng.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PENG_H
-#define _PENG_H
-
-#include "peng-base.h"
-#include "peng-int.h"
-#include "peng-bool.h"
-
-#endif
