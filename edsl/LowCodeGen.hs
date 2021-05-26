38700ea15f7167e5f95e7a80582cd6be6b917805
j-hui <j-hui@cs.columbia.edu>
2021-05-26 09:02:42 -0400

    Formatting (I used the wrong formatter previously)

--- edsl/LowCodeGen.hs
+++ edsl/LowCodeGen.hs
@@ -3,14 +3,19 @@
 
 module LowCodeGen where
 
-import Control.Monad.State.Lazy (State, evalState, gets, modify)
-import Data.Either (rights)
-import qualified Data.Map as Map
-import Language.C.Quote.GCC
-import qualified Language.C.Syntax as C
-import LowCore
-import Text.PrettyPrint.Mainland -- (Doc, pretty)
-import Text.PrettyPrint.Mainland.Class (pprList)
+import           Control.Monad.State.Lazy
+                 ( State, evalState, gets, modify )
+
+import           Data.Either                     ( rights )
+import qualified Data.Map                        as Map
+
+import           Language.C.Quote.GCC
+import qualified Language.C.Syntax               as C
+
+import           LowCore
+
+import           Text.PrettyPrint.Mainland -- (Doc, pretty)
+import           Text.PrettyPrint.Mainland.Class ( pprList )
 
 -- | Use snake_case for c literals
 {-# ANN module "HLint: ignore Use camelCase" #-}
@@ -21,23 +26,24 @@ compile_ :: Bool -> Maybe Int -> Program -> String
 compile_ wantMain tickLimit program = pretty 120 $ pprList compUnit
   where
     preamble = mkPreamble tickLimit
-    (decls, defns) = concat2 $ unzip $ map mkProcedure $ Map.elems (funs program)
 
-    compUnit = preamble ++ decls ++ defns ++ [mkMain program | wantMain]
+    (decls, defns) = concat2 $ unzip $ map mkProcedure $
+        Map.elems (funs program)
+
+    compUnit = preamble ++ decls ++ defns ++ [ mkMain program | wantMain ]
 
     concat2 (x, y) = (concat x, concat y)
 
 -- | State maintained while compiling a procedure
-data TRState = TRState
-  { -- | Procedure we are compiling
-    procedure :: Procedure,
-    -- | Which number has the next case?
-    ncase :: Int,
-    -- | The size of the widest wait
-    numwaits :: Int,
-    -- | Local references declared with var
-    locals :: [(String, Type)]
-  }
+data TRState = TRState   -- | Procedure we are compiling
+    { procedure :: Procedure
+      -- | Which number has the next case?
+    , ncase     :: Int
+      -- | The size of the widest wait
+    , numwaits  :: Int
+      -- | Local references declared with var
+    , locals    :: [(String, Type)]
+    }
 
 -- | Translation monad.
 type TR a = State TRState a
@@ -48,15 +54,15 @@ runTR p tra = evalState tra $ TRState p 0 0 []
 
 nextCase :: TR Int
 nextCase = do
-  n <- gets ncase
-  modify $ \st -> st {ncase = n + 1}
-  return n
+    n <- gets ncase
+    modify $ \st -> st { ncase = n + 1 }
+    return n
 
 addLocal :: String -> Type -> TR ()
-addLocal n t = modify $ \st -> st {locals = (n, t) : locals st}
+addLocal n t = modify $ \st -> st { locals = (n, t) : locals st }
 
 maxWaits :: Int -> TR ()
-maxWaits rs = modify $ \st -> st {numwaits = rs `max` numwaits st}
+maxWaits rs = modify $ \st -> st { numwaits = rs `max` numwaits st }
 
 type CIdent = String
 
@@ -159,7 +165,7 @@ act_ ty = [cty|typename $id:(actId ty)|]
 
 mkPreamble :: Maybe Int -> [C.Definition]
 mkPreamble tickLimit =
-  [cunit|
+    [cunit|
 $esc:("#include \"peng-platform.h\"")
 $esc:("#include \"peng.h\"")
 $esc:("#include <stdio.h>")
@@ -176,10 +182,10 @@ $ty:time_t limit = $exp:limit;
 static int __add(int a, int b) {
   return a + b;
 }
-
 |]
   where
     limit = maybe [cexp|ULONG_MAX|] promoteExp tickLimit
+
     promoteExp i = [cexp|$int:i|]
 
 topReturn :: C.Definition
@@ -191,7 +197,7 @@ topReturn = [cedecl| void $id:topReturnId($ty:act_t *act) { return; } |]
 --             (3) centralize debug logic
 mkMain :: Program -> C.Definition
 mkMain program =
-  [cedecl|
+    [cedecl|
     void main(void) {
       $ty:act_t top = { .step = $id:topReturnId };
 
@@ -216,43 +222,41 @@ mkMain program =
   |]
   where
     enter = enter_ $ main program
+
     enterArgs =
-      [ [cexp|&top|],
-        [cexp|PRIORITY_AT_ROOT|],
-        [cexp|DEPTH_AT_ROOT|]
-      ]
+        [ [cexp|&top|], [cexp|PRIORITY_AT_ROOT|], [cexp|DEPTH_AT_ROOT|] ]
         ++ map enterArg (args program)
 
     enterArg (Left ssmExp) = mkExp [] ssmExp -- FIXME: this is buggy if ssmExp contains a var
     enterArg (Right (ref, _)) = [cexp|&$id:ref|]
 
     refInits = concatMap refInit $ rights $ args program
+
     refPrints = map refPrint $ rights $ args program
 
-    refInit (ref, typ) =
-      [ [citem|$ty:(svt_ typ) $id:ref;|],
-        [citem|$id:(initialize_ typ)(&$id:ref);|],
-        [citem|$id:ref.value = /* FIXME */ 0;|]
-      ]
+    refInit (ref, typ) = [ [citem|$ty:(svt_ typ) $id:ref;|]
+                         , [citem|$id:(initialize_ typ)(&$id:ref);|]
+                         , [citem|$id:ref.value = /* FIXME */ 0;|]
+                         ]
 
-    refPrint (ref, _ {-typ-}) = [citem|printf($string:fmtString, /* FIXME */ (long) $id:ref.value);|]
+    refPrint (ref, _ {-typ-}) =
+        [citem|printf($string:fmtString, /* FIXME */ (long) $id:ref.value);|]
       where
         fmtString = "result " ++ ref ++ " %ld\n"
 
 mkProcedure :: Procedure -> ([C.Definition], [C.Definition])
 mkProcedure p = runTR p $ do
-  (stepDecl, stepDefn) <- mkStep
-  (enterDecl, enterDefn) <- mkEnter
-  structDefn <- mkStruct
-  return ([structDefn, enterDecl, stepDecl], [enterDefn, stepDefn])
+    (stepDecl, stepDefn) <- mkStep
+    (enterDecl, enterDefn) <- mkEnter
+    structDefn <- mkStruct
+    return ([ structDefn, enterDecl, stepDecl ], [ enterDefn, stepDefn ])
 
 mkStruct :: TR C.Definition
 mkStruct = do
-  p <- gets procedure
-  ts <- gets numwaits
-  ls <- gets locals
-  return
-    [cedecl|
+    p <- gets procedure
+    ts <- gets numwaits
+    ls <- gets locals
+    return [cedecl|
   typedef struct {
     $sdecls:aCTIVATION_RECORD_FIELDS
 
@@ -264,82 +268,84 @@ mkStruct = do
   |]
   where
     aCTIVATION_RECORD_FIELDS =
-      [ [csdecl|$ty:stepf_t *step;|],
-        [csdecl|struct act *caller;|],
-        [csdecl|$ty:uint16_t pc;|],
-        [csdecl|$ty:uint16_t children;|],
-        [csdecl|$ty:priority_t priority;|],
-        [csdecl|$ty:depth_t depth;|],
-        [csdecl|$ty:bool_t scheduled;|]
-      ]
+        [ [csdecl|$ty:stepf_t *step;|]
+        , [csdecl|struct act *caller;|]
+        , [csdecl|$ty:uint16_t pc;|]
+        , [csdecl|$ty:uint16_t children;|]
+        , [csdecl|$ty:priority_t priority;|]
+        , [csdecl|$ty:depth_t depth;|]
+        , [csdecl|$ty:bool_t scheduled;|]
+        ]
+
     param (n, t) = [csdecl|$ty:(param_ t) $id:n;|]
+
     local (n, t) = [csdecl|$ty:(svt_ t) $id:n;|]
+
     trig i = [csdecl|$ty:trigger_t $id:t;|]
       where
         t = "trig" ++ show i
 
 mkEnter :: TR (C.Definition, C.Definition)
 mkEnter = do
-  p <- gets procedure
-  ts <- gets numwaits
-  ls <- gets locals
+    p <- gets procedure
+    ts <- gets numwaits
+    ls <- gets locals
+    let act = act_ (name p)
+        enter = enter_ (name p)
+        step = stepId (name p)
+        params = [cparams|$ty:act *caller, $ty:priority_t priority, $ty:depth_t depth|]
+            ++ map param (arguments p)
+    return ( [cedecl| $ty:act *$id:enter($params:params);|]
+           , [cedecl|
+             $ty:act *$id:enter($params:params) {
+               $ty:act_t *gen_act = $id:act_enter(sizeof($ty:act), $id:step, caller, priority, depth);
+               $ty:act *act = ($ty:act *) gen_act;
 
-  let act = act_ (name p)
-      enter = enter_ (name p)
-      step = stepId (name p)
-      params =
-        [cparams|$ty:act *caller, $ty:priority_t priority, $ty:depth_t depth|]
-          ++ map param (arguments p)
-  return
-    ( [cedecl| $ty:act *$id:enter($params:params);|],
-      [cedecl|
-  $ty:act *$id:enter($params:params) {
-    $ty:act_t *gen_act = $id:act_enter(sizeof($ty:act), $id:step, caller, priority, depth);
-    $ty:act *act = ($ty:act *) gen_act;
+               $stms:(map initParam (arguments p))
+               $stms:(map initLocal ls)
+               $stms:(map initTrig [1..ts])
 
-    $stms:(map initParam (arguments p))
-    $stms:(map initLocal ls)
-    $stms:(map initTrig [1..ts])
-
-    return act;
-  }|]
-    )
+               return act;
+             }|]
+           )
   where
     param (n, t) = [cparam|$ty:(param_ t) $id:n|]
+
     -- initParam (n, Ref t) = -- TODO: do we need to treat refs differently?
     initParam (n, _) = [cstm|act->$id:n = $id:n;|]
+
     initLocal (n, t) = [cstm| $id:(initialize_ t)(&act->$id:n);|]
+
     initTrig i = [cstm| act->$id:trig.act = gen_act;|]
       where
         trig = "trig" ++ show i
 
 mkStep :: TR (C.Definition, C.Definition)
 mkStep = do
-  p <- gets procedure
-  _ <- nextCase -- Toss away 0th case
-  cases <- concat <$> mapM mkCase (body p)
-  refs <- gets locals
-  final <- nextCase
-  let step = stepId (name p)
-      act = act_ (name p)
-  return
-    ( [cedecl|void $id:step($ty:act_t *gen_act);|],
-      [cedecl|
-    void $id:step($ty:act_t *gen_act) {
-      $ty:act *act = ($ty:act *) gen_act; /* FIXME: remove cast */
-      switch (gen_act->pc) {
-      case 0:;
-        $stms:cases
+    p <- gets procedure
+    _ <- nextCase -- Toss away 0th case
+    cases <- concat <$> mapM mkCase (body p)
+    refs <- gets locals
+    final <- nextCase
+    let step = stepId (name p)
+        act = act_ (name p)
+    return ( [cedecl|void $id:step($ty:act_t *gen_act);|]
+           , [cedecl|
+             void $id:step($ty:act_t *gen_act) {
+               $ty:act *act = ($ty:act *) gen_act; /* FIXME: remove cast */
+               switch (gen_act->pc) {
+               case 0:;
+                 $stms:cases
 
-      case $int:final:; /* FIXME: this last case is not needed */
-        $stms:(map dequeue refs)
-        leave(gen_act, sizeof($ty:act));
-        return;
-      }
-      printf("Error: Unreachable\n");
-      assert(0);
-    }|]
-    )
+               case $int:final:; /* FIXME: this last case is not needed */
+                 $stms:(map dequeue refs)
+                 leave(gen_act, sizeof($ty:act));
+                 return;
+               }
+               printf("Error: Unreachable\n");
+               assert(0);
+             }|]
+           )
   where
     dequeue (s, _) = [cstm|$id:dequeue_event(($ty:sv_t *) &act->$id:s);|]
 
@@ -352,112 +358,106 @@ mkStep = do
 -- TODOs: remove hard-coded `act` stuff
 mkCase :: Stm -> TR [C.Stm]
 mkCase (NewRef n t v) = do
-  params <- map fst . arguments <$> gets procedure
-  let lvar = getVarName n
-      lhs = [cexp|&act->$id:lvar|]
-      rhs = mkExp params v
-  addLocal lvar t
-  return [[cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|]]
+    params <- map fst . arguments <$> gets procedure
+    let lvar = getVarName n
+        lhs = [cexp|&act->$id:lvar|]
+        rhs = mkExp params v
+    addLocal lvar t
+    return [ [cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|] ]
 mkCase (GetRef n t (rvar, _)) = do
-  refs <- map fst <$> gets locals
-  let lvar = getVarName n
-      lhs = [cexp|&act->$id:lvar|]
-      rhs =
-        if rvar `elem` refs
-          then [cexp|act->$id:rvar.value|]
-          else [cexp|act->$id:rvar->value|]
-  return [[cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|]]
+    refs <- map fst <$> gets locals
+    let lvar = getVarName n
+        lhs = [cexp|&act->$id:lvar|]
+        rhs = if rvar `elem` refs
+              then [cexp|act->$id:rvar.value|]
+              else [cexp|act->$id:rvar->value|]
+    return [ [cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|] ]
 mkCase (SetRef (lvar, t) e) = do
-  params <- map fst . arguments <$> gets procedure
-  refs <- map fst <$> gets locals
-  let lhs =
-        if lvar `elem` refs
-          then [cexp|&act->$id:lvar|]
-          else [cexp|act->$id:lvar|]
-      rhs = mkExp params e
-  return [[cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|]]
+    params <- map fst . arguments <$> gets procedure
+    refs <- map fst <$> gets locals
+    let lhs = if lvar `elem` refs
+              then [cexp|&act->$id:lvar|]
+              else [cexp|act->$id:lvar|]
+        rhs = mkExp params e
+    return [ [cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|] ]
 mkCase (SetLocal n t e) = do
-  params <- map fst . arguments <$> gets procedure
-  let lvar = getVarName n
-      lhs = [cexp|&act->$id:lvar|]
-      rhs = mkExp params e
-  return [[cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|]]
+    params <- map fst . arguments <$> gets procedure
+    let lvar = getVarName n
+        lhs = [cexp|&act->$id:lvar|]
+        rhs = mkExp params e
+    return [ [cstm|$id:(assign_ t)($exp:lhs, gen_act->priority, $exp:rhs);|] ]
 mkCase (If c t e) = do
-  params <- map fst . arguments <$> gets procedure
-  let cnd = mkExp params c
-  thn <- concat <$> mapM mkCase t
-  els <- concat <$> mapM mkCase e
-  return [[cstm| if ($exp:cnd) { $stms:thn } else { $stms:els }|]]
+    params <- map fst . arguments <$> gets procedure
+    let cnd = mkExp params c
+    thn <- concat <$> mapM mkCase t
+    els <- concat <$> mapM mkCase e
+    return [ [cstm| if ($exp:cnd) { $stms:thn } else { $stms:els }|] ]
 mkCase (While c b) = do
-  params <- map fst . arguments <$> gets procedure
-  let cnd = mkExp params c
-  bod <- concat <$> mapM mkCase b
-  return [[cstm| while ($exp:cnd) { $stms:bod } |]]
+    params <- map fst . arguments <$> gets procedure
+    let cnd = mkExp params c
+    bod <- concat <$> mapM mkCase b
+    return [ [cstm| while ($exp:cnd) { $stms:bod } |] ]
 mkCase (After d (lvar, t) v) = do
-  params <- map fst . arguments <$> gets procedure
-  refs <- map fst <$> gets locals
-  let del = mkExp params d
-      lhs =
-        if lvar `elem` refs
-          then [cexp|&act->$id:lvar|]
-          else [cexp|act->$id:lvar|]
-      rhs = mkExp params v
-  return [[cstm| $id:(later_ t)($exp:lhs, $exp:del, $exp:rhs);|]]
+    params <- map fst . arguments <$> gets procedure
+    refs <- map fst <$> gets locals
+    let del = mkExp params d
+        lhs = if lvar `elem` refs
+              then [cexp|&act->$id:lvar|]
+              else [cexp|act->$id:lvar|]
+        rhs = mkExp params v
+    return [ [cstm| $id:(later_ t)($exp:lhs, $exp:del, $exp:rhs);|] ]
 mkCase (Changed n t (rvar, _)) = do
-  refs <- map fst <$> gets locals
-  let lvar = getVarName n
-      lhs = [cexp|&act->$id:lvar|]
-      rhs =
-        if rvar `elem` refs
-          then [cexp|&act->$id:rvar|]
-          else [cexp|act->$id:rvar|]
-  return [[cstm| $id:(assign_ t)($exp:lhs, act->priority, $id:event_on(($ty:sv_t *) $exp:rhs));|]]
+    refs <- map fst <$> gets locals
+    let lvar = getVarName n
+        lhs = [cexp|&act->$id:lvar|]
+        rhs = if rvar `elem` refs
+              then [cexp|&act->$id:rvar|]
+              else [cexp|act->$id:rvar|]
+    return [ [cstm| $id:(assign_ t)($exp:lhs, act->priority, $id:event_on(($ty:sv_t *) $exp:rhs));|]
+           ]
 mkCase (Wait ts) = do
-  caseNum <- nextCase
-  maxWaits $ length ts
-  refs <- map fst <$> gets locals
-  let trigs = zip [1 ..] $ map (mkTrig refs) ts
-  return $
-    fmap sensitizeTrig trigs
-      ++ [ [cstm| gen_act->pc = $int:caseNum; |],
-           [cstm| return; |],
-           [cstm| case $int:caseNum: ; |]
-         ]
-      ++ fmap desensitizeTrig trigs
+    caseNum <- nextCase
+    maxWaits $ length ts
+    refs <- map fst <$> gets locals
+    let trigs = zip [ 1 .. ] $ map (mkTrig refs) ts
+    return $ fmap sensitizeTrig trigs
+        ++ [ [cstm| gen_act->pc = $int:caseNum; |]
+           , [cstm| return; |]
+           , [cstm| case $int:caseNum: ; |]
+           ] ++ fmap desensitizeTrig trigs
   where
-    sensitizeTrig (i, trig) = [cstm|$id:sensitize(($ty:sv_t *) $exp:trig, &act->$id:(trig_ i));|]
-    desensitizeTrig (i, _) = [cstm|$id:desensitize(&act->$id:(trig_ i));|]
-    mkTrig refs (trig, _) =
-      if trig `elem` refs
-        then [cexp|&act->$id:trig|]
-        else [cexp|act->$id:trig|]
-mkCase (Fork cs) = do
-  params <- map fst . arguments <$> gets procedure
-  refs <- map fst <$> gets locals
-  caseNum <- nextCase
-  let depthSub = (ceiling $ logBase (2 :: Double) $ fromIntegral $ length cs) :: Integer
-      newDepth = [cexp|act->depth - $int:depthSub|]
-      mkArgs i extraArgs =
-        [ [cexp|act|],
-          [cexp|act->priority + $int:i * (1 << $exp:newDepth)|],
-          newDepth
-        ]
-          ++ extraArgs
+    sensitizeTrig (i, trig) =
+        [cstm|$id:sensitize(($ty:sv_t *) $exp:trig, &act->$id:(trig_ i));|]
 
-      mkCall i (r, as) = [cstm|$id:fork(($ty:act_t *) $id:(enter_ r)($args:enterArgs));|]
-        where
-          enterArgs = mkArgs i $ map mkArg as
-      mkArg (Left e) = mkExp params e
-      mkArg (Right (r, _)) =
-        if r `elem` refs
-          then [cexp|&act->$id:r|]
-          else [cexp|act->$id:r|]
-  return $
-    zipWith mkCall [1 :: Int ..] cs
-      ++ [ [cstm| gen_act->pc = $int:caseNum; |],
-           [cstm| return; |],
-           [cstm| case $int:caseNum: ; |]
-         ]
+    desensitizeTrig (i, _) = [cstm|$id:desensitize(&act->$id:(trig_ i));|]
+
+    mkTrig refs (trig, _) = if trig `elem` refs
+                            then [cexp|&act->$id:trig|]
+                            else [cexp|act->$id:trig|]
+mkCase (Fork cs) = do
+    params <- map fst . arguments <$> gets procedure
+    refs <- map fst <$> gets locals
+    caseNum <- nextCase
+    let depthSub = (ceiling $ logBase (2 :: Double) $ fromIntegral $ length cs)
+            :: Integer
+        newDepth = [cexp|act->depth - $int:depthSub|]
+        mkArgs i extraArgs =
+            [ [cexp|act|]
+            , [cexp|act->priority + $int:i * (1 << $exp:newDepth)|]
+            , newDepth
+            ] ++ extraArgs
+        mkCall i (r, as) =
+            [cstm|$id:fork(($ty:act_t *) $id:(enter_ r)($args:enterArgs));|]
+          where
+            enterArgs = mkArgs i $ map mkArg as
+        mkArg (Left e) = mkExp params e
+        mkArg (Right (r, _)) =
+            if r `elem` refs then [cexp|&act->$id:r|] else [cexp|act->$id:r|]
+    return $ zipWith mkCall [ 1 :: Int .. ] cs
+        ++ [ [cstm| gen_act->pc = $int:caseNum; |]
+           , [cstm| return; |]
+           , [cstm| case $int:caseNum: ; |]
+           ]
 mkCase Skip = return []
 
 -- TODO: double check that the quasi quote thing handles parenthesization and
@@ -469,12 +469,15 @@ mkExp _ (Lit _ (LInt64 i)) = [cexp|$int:i|]
 mkExp _ (Lit _ (LUInt64 i)) = [cexp|$int:i|]
 mkExp _ (Lit _ (LBool True)) = [cexp|true|]
 mkExp _ (Lit _ (LBool False)) = [cexp|false|]
-mkExp params (Var _ n) = if n `elem` params then [cexp|act->$id:n|] else [cexp|act->$id:n.value|]
+mkExp params (Var _ n) =
+    if n `elem` params then [cexp|act->$id:n|] else [cexp|act->$id:n.value|]
 mkExp params (UOp _ e Neg) = [cexp|- $exp:(mkExp params e)|]
-mkExp params (BOp TInt32 e1 e2 OPlus) = [cexp|__add($exp:(mkExp params e1), $exp:(mkExp params e2))|]
+mkExp params (BOp TInt32 e1 e2 OPlus) =
+    [cexp|__add($exp:(mkExp params e1), $exp:(mkExp params e2))|]
 mkExp params (BOp _ e1 e2 op) = c op
   where
     (c1, c2) = (mkExp params e1, mkExp params e2)
+
     c OPlus = [cexp|$exp:c1 + $exp:c2|]
     c OMinus = [cexp|$exp:c1 - $exp:c2|]
     c OTimes = [cexp|$exp:c1 * $exp:c2|]
