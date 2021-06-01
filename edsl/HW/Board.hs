5d9b22421025e8c55a23c4c00d23c6cb676f777b
Robert Krook <krookr@chalmers.se>
2021-06-01 17:22:40 +0200

    pushed notes that I had written before the meeting

--- edsl/HW/Board.hs
+++ edsl/HW/Board.hs
@@ -1,4 +1,102 @@
-module Board where
+module HW.Board where
+
+import SSM
+
+--data Board -- some representation of a board and the peripherals on it
+           -- such as leds, buttons, uart, BT.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--nrf52840dk :: SSM Board
+--nrf52840dk = undefined
+
+-- | This call makes sure that the code to initialize & use LEDs is executed
+-- Since our AST is untyped we can create a value that represents the properties
+-- of a led, and then pretend that it's a normal reference to a boolean.
+getLed :: Int -> SSM (Ref Bool)
+getLed = undefined
+
+f :: Ref Bool -> SSM ()
+f r = after 1 r true'
+{-
+do -- this call performs all the necessary boilerplate and returns
+   -- a reference that can access the led
+   led <- getLed 1 -- led :: Ref Bool
+
+   -- this call returns a reference to the already created led
+   led' <- getLed 1
+
+   -- while this does what the first call to `getLed 1 board` did, but for
+   -- another led
+   led'' <- getLed 2 board
+
+  -- in the EDSL we use the reference as any 'normal' reference. The fact that
+  -- it manages some underlying resource is not a detail we want to clutter
+  -- the code with.
+  after 50 led true'
+  wait [led]
+  after 50 led false'
+  wait [led]
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 {-
 
