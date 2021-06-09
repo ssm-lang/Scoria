4cc0f6e86229a81935cf413c0680cc26f4da845a
j-hui <j-hui@cs.columbia.edu>
2021-06-09 16:55:31 -0400

    Added documentation for trace platform

--- platform/trace/Build.mk
+++ platform/trace/Build.mk
@@ -1,5 +1,11 @@
 ##
-# Test platform build rules
+# Trace platform build rules
+#
+# This platform is meant to be used in conjunction with this project's
+# Quickcheck property-based tests. When compiled with this platform, the
+# resulting executable will run on the host machine, and print a trace to stdout
+# that can be parsed and compared against the execution trace of the SSM
+# interpreter.
 ##
 
 CC = gcc
