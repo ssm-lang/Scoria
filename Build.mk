7dade3b44f7cd8b70531a66b6e83017deccfe8bc
j-hui <j-hui@cs.columbia.edu>
2021-06-18 18:35:27 -0400

    Allow top-level Build.mk to be included by others
    Now the top-level Build.mk is responsible for creating the build
    directory, meaning it can be included from other Makefiles (even ones
    beyond the source tree).

--- Build.mk
+++ Build.mk
@@ -1,21 +1,103 @@
 ##
-# Compilation rules common to all parts of the build system. This should be
-# included last by the main Makefile.
+# Top-level Build.mk. Designed to be included by a Makefile that defines the
+# following variables:
 #
-# This file defines compiler flags which causes the C compiler to generate a .d
-# file which lists the dependency information on other header files. On
-# subsequent builds, these .d files may be included by Make to attach additional
-# dependencies to .o files, which will force them to be rebuilt if header files
-# they depend on have been updated.
+# - PLATFORM: the platform for which targets should be built.
+# - SSMDIR: the root of the SSM source tree (i.e., where this file is).
 #
-# The list of dependency files is determined using the SRCS variable, which
-# should be populated by each of the other build directories (which is why this
-# should be included last). The LIBS variable is similarly used to determine
-# libraries that the main target should depend on.
+# ---
+#
+# Build directory setup based on Paul D. Smith's multi-platform recommendation:
+# http://make.mad-scientist.net/papers/multi-architecture-builds
+#
+# If make isn't invoked from inside a build directory, this will set it up
+# according to the specific platform, and re-invoke itself from within.
+# The build directory is located at build/$(PLATFORM). This Makefile also sets
+# up clean targets to remove the build directory.
+#
+# It will set up the following variables to help other Makefiles orient
+# themselves:
+#
+#   SSMDIR := absolute path of the directory where this Makefile lives
+#   PLATFORMDIR := platform-specific directory
+#   RUNTIMEDIR := directory where platform-generic runtime is located
+#
+# ---
 #
 # Auto-dependency generation based on technique described by Paul D. Smith:
 # http://make.mad-scientist.net/papers/advanced-auto-dependency-generation/
+#
+# Header dependencies are generated for any .c source files added to the SRCS
+# variable. It does so by specifying compiler flags which cause the
+# C preprocessor to generate .d files containing dependency information.
+# On subsequent builds, these .d files may be included by Make to attach
+# additional dependencies to .o targets, forcing them to be rebuilt if header
+# files they depend on have been updated.
+#
+# The list of dependency files is determined using the SRCS variable, which
+# should be populated by each of the other build directories. The LIBS variable
+# is similarly used to determine libraries that the main target should depend
+# on.
 ##
+
+ifndef PLATFORM
+$(error PLATFORM is not defined.)
+endif
+
+ifndef SSMDIR
+$(error SSMDIR is not defined.)
+endif
+
+# If make isn't invoked from inside a build directory, set it up and go there
+ifeq (,$(filter build, $(notdir $(abspath $(CURDIR)/..))))
+
+export PLATFORM
+export SSMDIR
+
+# No built-in rules for this Makefile.
+.SUFFIXES:
+
+BUILDDIR := $(CURDIR)/build/$(PLATFORM)
+
+# Set up build directory and recursively invoke Makefile from within, with the
+# same goals.
+.PHONY: $(BUILDDIR)
+$(BUILDDIR):
+	@+[ -d $(BUILDDIR) ] || mkdir -p $(BUILDDIR)
+	@+$(MAKE) -C $(BUILDDIR) -f $(CURDIR)/Makefile $(MAKECMDGOALS)
+
+# Do nothing for these targets, overriding the match-all rule defined below to
+# prevent recursive evaluation.
+Makefile : ;
+%.mk :: ;
+
+# Match-all rule that jumps to BUILDDIR rule above, and does nothing else.
+% :: $(BUILDDIR) ; @:
+
+# Create build directory, and echo the path.
+.PHONY: make_builddir
+make_builddir:
+	@+[ -d $(BUILDDIR) ] || mkdir -p $(BUILDDIR)
+	@+echo $(BUILDDIR)
+
+# Clean up by removing platform build directory. Note that this leaves other
+# platforms' build directories alone.
+.PHONY: clean
+clean:
+	$(RM) -r $(BUILDDIR)
+
+# Clean build directories for all platforms.
+.PHONY: distclean
+distclean:
+	$(RM) -r build
+
+else # make was invoked from inside a build directory
+
+RUNTIMEDIR := $(SSMDIR)/runtime
+PLATFORMDIR := $(SSMDIR)/platform/$(PLATFORM)
+
+include $(RUNTIMEDIR)/Build.mk
+include $(PLATFORMDIR)/Build.mk
 
 # Generated C files are placed in the build directory.
 GEN_SRCS := $(notdir $(wildcard *.c))
@@ -75,3 +157,5 @@ LDFLAGS += -L.
 
 # %.h : %.pen
 # 	$(PENG) --module-name=$(MODULE) --generate-h $< > $*.h
+
+endif
--- Makefile
+++ Makefile
@@ -1,84 +1,13 @@
 ##
-# Master Makefile. Sets up build directory for multi-platform build environment.
-# Based on Paul D. Smith's multi-architecture build recommendation:
-# http://make.mad-scientist.net/papers/multi-architecture-builds
-#
-# If this Makefile isn't invoked from inside the build directory, this will
-# determine the platform being built for, and set up the build environment
-# accordingly. Finally, it invoke itself again, from within the build directory.
-#
-# It will set up the following variables to help other Makefiles orient
-# themselves:
-#
-#   ROOTDIR := absolute path of the directory where this Makefile lives
-#   PLATFORM := name of the platform being targeted
-#   PLATFORMDIR := platform-specific directory
-#   RUNTIMEDIR := directory where platform-generic runtime is located
-#
-# The build directory is located at build/$(PLATFORM). This Makefile also sets
-# up a clean target to remove the build directory.
-#
-# Header dependencies are generated for any .c source files added to the SRCS
-# variable. See ./Build.mk for details.
+# SSM project Makefile, which provides entry point to top-level Build.mk.
 ##
-
-ifeq (,$(filter build, $(notdir $(abspath $(CURDIR)/..))))
-# If make isn't invoked from inside a build directory, set it up and go there
-
-# No built-in rules for this Makefile.
-.SUFFIXES:
 
 ifndef PLATFORM
 PLATFORM := simulation
-export PLATFORM
 endif
 
-ROOTDIR := $(CURDIR)
-export ROOTDIR
-
-BUILDDIR := build/$(PLATFORM)
-
-# Set up build directory and recursively invoke Makefile from within, with the
-# same goals.
-.PHONY: $(BUILDDIR)
-$(BUILDDIR):
-	@+[ -d $(BUILDDIR) ] || mkdir -p $(BUILDDIR)
-	@+$(MAKE) -C $(BUILDDIR) -f $(CURDIR)/Makefile $(MAKECMDGOALS)
-
-# Do nothing for these targets, overriding the match-all rule defined below to
-# prevent recursive evaluation.
-Makefile : ;
-%.mk :: ;
-
-# Match-all rule that jumps to BUILDDIR rule above, and does nothing else.
-% :: $(BUILDDIR) ; @:
-
-# Create build directory, and echo the path.
-.PHONY: make_builddir
-make_builddir:
-	@+[ -d $(BUILDDIR) ] || mkdir -p $(BUILDDIR)
-	@+echo $(BUILDDIR)
-
-# Clean up by removing platform build directory. Note that this leaves other
-# platforms' build directories alone.
-.PHONY: clean
-clean:
-	$(RM) -r $(BUILDDIR)
-
-# Clean build directories for all platforms.
-.PHONY: distclean
-distclean:
-	$(RM) -r build
-
-else # make was invoked from inside a build directory
-
-RUNTIMEDIR := $(ROOTDIR)/runtime
-PLATFORMDIR := $(ROOTDIR)/platform/$(PLATFORM)
-
-include $(RUNTIMEDIR)/Build.mk
-
-include $(PLATFORMDIR)/Build.mk
-
-include $(ROOTDIR)/Build.mk
-
+ifndef SSMDIR
+SSMDIR := $(CURDIR)
 endif
+
+include $(SSMDIR)/Build.mk
