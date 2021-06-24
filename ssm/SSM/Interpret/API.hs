946e26d728ba4105abe5039352fdd4196f191a84
Robert Krook <krookr@chalmers.se>
2021-06-24 21:19:39 +0200

    removed unused application file and made some small changes. Not too sure about the small changes

--- ssm/SSM/Interpret/API.hs
+++ ssm/SSM/Interpret/API.hs
@@ -66,6 +66,7 @@ import qualified Data.IntMap as IntMap
 import Data.STRef.Lazy
 import Data.Word
 import Data.Maybe
+import Data.List
 
 import Control.Monad.State.Lazy
 import Control.Monad.Writer.Lazy
@@ -163,7 +164,7 @@ fork :: (String, [Either SSMExp Reference])  -- ^ Procedure to fork (name and ar
 fork (n,args) prio dep par = do
     p <- lookupProcedure n
     variables <- params $ (fst . unzip . arguments) p
-    enqueue $ Proc prio dep 0 (Just par) variables Map.empty Nothing (body p)
+    enqueue $ mkProc prio dep 0 (Just par) variables Map.empty Nothing (body p)
   where
       -- | Return an initial variable storage for the new process. Expression arguments are turned into
       -- new STRefs while reference arguments are passed from the calling processes variable storage.
@@ -241,6 +242,50 @@ performEvents = do
           
           -- perform the actual update, eventually scheduling processes
           writeVar_ e (fromJust mv) (-1)
+
+schedule_event :: Reference -> Word64 -> SSMExp -> Interp s ()
+schedule_event r thn val = do
+    st <- get
+
+    e <- lookupRef (refName r)
+    
+    when (SSM.Interpret.Internal.now st > thn) $ error "bad after"
+
+    -- fetch ref so we can update the scheduled information
+    (ref,pr,b,mt,_) <- lift' $ readSTRef e
+    lift' $ writeSTRef e (ref,pr,b,Just thn, Just val)
+    
+    -- if it was scheduled before we remove the old one from the eventqueue
+    -- and just insert it again.
+    if isJust mt
+        then do let newevs = insert_event thn e (delete_event (fromJust mt) e (events st))
+                modify $ \st -> st { events = newevs }
+    -- otherwise we check if the queue is full before we schedule the new event
+        else if numevents st == eventqueueSize
+            then error "eventqueue full"
+            else do let es' = insert_event thn e (events st)
+                    modify $ \st -> st { events    = es'
+                                       , numevents = numevents st + 1
+                                       }
+  where
+      insert_event :: Word64 -> Var s -> Map.Map Word64 [Var s] -> Map.Map Word64 [Var s]
+      insert_event when v m = adjustWithDefault (v :) [v] when m
+        where
+            adjustWithDefault :: Ord k => (a -> a) -> a -> k -> Map.Map k a -> Map.Map k a
+            adjustWithDefault f v k m =
+                if Map.member k m
+                    then Map.adjust f k m
+                    else Map.insert k v m
+
+delete_event :: Word64 -> Var s -> Map.Map Word64 [Var s] -> Map.Map Word64 [Var s]
+delete_event when v m = case Map.lookup when m of
+    Just [x] -> if x == v then Map.delete when m else m
+    Just x   -> Map.adjust (delete v) when m
+    Nothing  -> m
+
+delete_events :: [(Word64, Var s)] -> Map.Map Word64 [Var s] -> Map.Map Word64 [Var s]
+delete_events [] m         = m
+delete_events ((t,v):xs) m = delete_events xs $ delete_event t v m
 
 -- | Inspects the eventqueue and returns the next event time.
 nextEventTime :: Interp s Word64
