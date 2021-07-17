19c98914be1f9d5e0e4fff1add99e76a94afc5b1
j-hui <j-hui@cs.columbia.edu>
2021-07-16 23:55:18 -0400

    Added new test case for infinite recursion

---
+++ test/regression-low/Regression/RecurseForeverSpec.hs
@@ -0,0 +1,25 @@
+module Regression.RecurseForeverSpec where
+
+import           Data.Map                       ( fromList )
+import           SSM.Core.Syntax
+import qualified Test.Hspec                    as H
+import qualified Test.Hspec.QuickCheck         as H
+import qualified Test.SSM.Prop                 as T
+
+p :: Program
+p = Program
+  { entry = "fun0"
+  , args  = []
+  , funs  = fromList
+              [ ( "fun0"
+                , Procedure
+                  { name      = "fun0"
+                  , arguments = []
+                  , body      = [ Fork [("fun0", [])] ]
+                  }
+                )
+              ]
+  }
+
+spec :: H.Spec
+spec = T.correctSpec "RecurseForever" p
