a8985958cc540a9c072974302a8d6b1007b75146
j-hui <j-hui@cs.columbia.edu>
2021-07-18 18:16:05 -0400

    Added entry point and documentation to LaterAssignOverwrite

--- test/regression-low/Regression/LaterAssignOverwriteSpec.hs
+++ test/regression-low/Regression/LaterAssignOverwriteSpec.hs
@@ -1,3 +1,12 @@
+-- | Whether an instantaneous assignment overwrites a previously scheduled
+-- delayed assignment.
+--
+-- fun1 (r1: &bool, r3: &i32) =
+--   after 2, r1 <- True
+--   after 1, r3 <- 3
+--   r3 <- 4
+--   wait r1
+--   wait r3
 module Regression.LaterAssignOverwriteSpec where
 
 import           Data.Map                       ( fromList )
@@ -8,26 +17,38 @@ import qualified Test.SSM.Prop                 as T
 
 p :: Program
 p = Program
-  { entry = "fun1"
-  , args  = [Right ("ref1", Ref TBool), Right ("ref3", Ref TInt32)]
+  { entry = "fun0"
+  , args  = []
   , funs  = fromList
-              [ ( "fun1"
-                , Procedure
-                  { name = "fun1"
-                  , arguments = [("ref1", Ref TBool), ("ref3", Ref TInt32)]
-                  , body = [ After (Lit TUInt64 (LUInt64 2))
-                                   ("ref1", Ref TBool)
-                                   (Lit TBool (LBool True))
-                           , After (Lit TUInt64 (LUInt64 1))
-                                   ("ref3", Ref TInt32)
-                                   (Lit TInt32 (LInt32 3))
-                           , SetRef ("ref3", Ref TInt32) (Lit TInt32 (LInt32 4))
-                           , Wait [("ref1", Ref TBool)]
-                           , Wait [("ref3", Ref TInt32)]
-                           ]
-                  }
-                )
-              ]
+    [ ( "fun0"
+      , Procedure
+        { name      = "fun0"
+        , arguments = []
+        , body      =
+          [ NewRef (Fresh "ref1") (Ref TBool)  (Lit TBool (LBool True))
+          , NewRef (Fresh "ref3") (Ref TInt32) (Lit TInt32 (LInt32 0))
+          , Fork
+            [("fun1", [Right ("ref1", Ref TBool), Right ("ref3", Ref TInt32)])]
+          ]
+        }
+      )
+    , ( "fun1"
+      , Procedure
+        { name      = "fun1"
+        , arguments = [("ref1", Ref TBool), ("ref3", Ref TInt32)]
+        , body      = [ After (Lit TUInt64 (LUInt64 2))
+                              ("ref1", Ref TBool)
+                              (Lit TBool (LBool True))
+                      , After (Lit TUInt64 (LUInt64 1))
+                              ("ref3", Ref TInt32)
+                              (Lit TInt32 (LInt32 3))
+                      , SetRef ("ref3", Ref TInt32) (Lit TInt32 (LInt32 4))
+                      , Wait [("ref1", Ref TBool)]
+                      , Wait [("ref3", Ref TInt32)]
+                      ]
+        }
+      )
+    ]
   }
 
 spec :: H.Spec
