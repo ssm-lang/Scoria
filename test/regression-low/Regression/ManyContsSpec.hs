d70cadd56da304f15c1a14eaa74faa35ce6f31a2
j-hui <j-hui@cs.columbia.edu>
2021-07-18 18:20:47 -0400

    Added entry point and documentation to ManyConts

--- test/regression-low/Regression/ManyContsSpec.hs
+++ test/regression-low/Regression/ManyContsSpec.hs
@@ -1,3 +1,5 @@
+-- | Forkbomb to fill up the continuation queue, but with some delay at each
+-- fork.
 module Regression.ManyContsSpec where
 
 import           Data.Map                       ( fromList )
@@ -8,39 +10,37 @@ import qualified Test.SSM.Prop                 as T
 
 p :: Program
 p = Program
-  { entry = "fun1"
-  , args  = [ Right ("ref2", Ref TUInt64)
-            ]
+  { entry = "fun0"
+  , args  = []
   , funs  = fromList
-    [ ( "fun1"
-      , Procedure
-        { name      = "fun1"
-        , arguments = [ ("ref2", Ref TUInt64)
-                      ]
-        , body      =
-          [ After
-            (Lit TUInt64 (LUInt64 2))
-            ("ref2", Ref TUInt64)
-            (Lit TUInt64 (LUInt64 2))
-          , Wait [("ref2", Ref TUInt64)]
-          , Fork
-            [ ( "fun1"
-              , [ Right ("ref2", Ref TUInt64)
-                ]
-              )
-            , ( "fun1"
-              , [ Right ("ref2", Ref TUInt64)
-                ]
-              )
-            , ( "fun1"
-              , [ Right ("ref2", Ref TUInt64)
-                ]
-              )
-            ]
-          ]
-        }
-      )
-    ]
+              [ ( "fun0"
+                , Procedure
+                  { name      = "fun0"
+                  , arguments = []
+                  , body      = [ NewRef (Fresh "ref2")
+                                         (Ref TUInt64)
+                                         (Lit TUInt64 (LUInt64 0))
+                                , Fork [("fun1", [Right ("ref2", Ref TUInt64)])]
+                                ]
+                  }
+                )
+              , ( "fun1"
+                , Procedure
+                  { name      = "fun1"
+                  , arguments = [("ref2", Ref TUInt64)]
+                  , body      = [ After (Lit TUInt64 (LUInt64 2))
+                                        ("ref2", Ref TUInt64)
+                                        (Lit TUInt64 (LUInt64 2))
+                                , Wait [("ref2", Ref TUInt64)]
+                                , Fork
+                                  [ ("fun1", [Right ("ref2", Ref TUInt64)])
+                                  , ("fun1", [Right ("ref2", Ref TUInt64)])
+                                  , ("fun1", [Right ("ref2", Ref TUInt64)])
+                                  ]
+                                ]
+                  }
+                )
+              ]
   }
 
 spec :: H.Spec
