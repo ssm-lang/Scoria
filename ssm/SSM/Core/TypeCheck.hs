0d4dc23ce4d06223c54eeadf5d2cd0d286791180
Yalu Cai <yc468@duke.edu>
2021-08-19 18:50:11 -0400

    Later Assign

--- ssm/SSM/Core/TypeCheck.hs
+++ ssm/SSM/Core/TypeCheck.hs
@@ -247,7 +247,7 @@ typeCheckStm (SetRef ref expr) env = do
     expMatchRef ref expr env
     return env
 typeCheckStm (NewRef name ty expr) env
-    | actualTy == ty = Right (Map.insert name RefEntry {ty=ty} env)
+    | actualTy == ty = Right (Map.insert name RefEntry {ty=Ref ty} env)
     | otherwise =
         Left TypeError {expected=ty, actual=actualTy, msg="Expression type doesn't match the reference"}
     where
