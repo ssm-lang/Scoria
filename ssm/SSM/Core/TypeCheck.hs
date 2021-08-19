67b8a7671026ccd54b17b4bcbb5069e72510ca2c
Yalu Cai <yc468@duke.edu>
2021-08-19 17:59:10 -0400

    Later Assign

--- ssm/SSM/Core/TypeCheck.hs
+++ ssm/SSM/Core/TypeCheck.hs
@@ -81,7 +81,7 @@ typeCheckRef env (Dynamic (ident, ty)) =
         Nothing -> Left TypeError {expected=ty, actual=TUInt8, msg="The reference " ++ show ident ++ " is undefined"}
         Just RefEntry {ty=t} ->
             if Ref t == ty then Right ty else
-                Left TypeError {expected=t, actual=ty, msg="The stored reference doesn't match its claimed type"}
+                Left TypeError {expected=t, actual=ty, msg="The stored reference doesn't match its claimed type. Expected: " ++ show t ++ " Actual: " ++ show ty}
         Just ProcEntry {params=_} -> Left TypeError {expected=ty, actual=TUInt8, msg="The name " ++ show ident ++ " is for a function, not reference"}
         Just VarEntry {ty=_} -> Left TypeError {expected=ty, actual=TUInt8, msg="The name " ++ show ident ++ " is for a variable, not reference"}
 typeCheckRef env (Static (ident, ty)) = 
@@ -89,7 +89,7 @@ typeCheckRef env (Static (ident, ty)) =
         Nothing -> Left TypeError {expected=ty, actual=TUInt8, msg="The reference is undefined"}
         Just RefEntry {ty=t} ->
             if Ref t == ty then Right ty else
-                Left TypeError {expected=t, actual=ty, msg="The stored reference doesn't match its claimed type"}
+                Left TypeError {expected=t, actual=ty, msg="The stored reference doesn't match its claimed type. Expected: " ++ show t ++ " Actual: " ++ show ty}
         Just _ -> Left TypeError {expected=ty, actual=TUInt8, msg="The name is for a function or variable, not reference"}
 
 -- | Checks whether an argument has the correct type
