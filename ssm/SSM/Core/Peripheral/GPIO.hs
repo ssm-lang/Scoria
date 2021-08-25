d1e223d75943f5215a0d9dbaa3816d129bf06946
Robert Krook <krookr@chalmers.se>
2021-08-25 17:49:45 +0200

    formatting

--- ssm/SSM/Core/Peripheral/GPIO.hs
+++ ssm/SSM/Core/Peripheral/GPIO.hs
@@ -13,22 +13,22 @@ describe which pins are being used, what type of pin they are and what names the
 assigned. Regardless of how the frontend language lets the programmer interact with GPIO,
 this describes how the core syntax interacts with GPIO. -}
 module SSM.Core.Peripheral.GPIO
-    ( GPIOPeripheral
-    , switchpins
-    , emptyGPIOPeripheral
-    , addSwitchGPIO
-    ) where
+  ( GPIOPeripheral
+  , switchpins
+  , emptyGPIOPeripheral
+  , addSwitchGPIO
+  ) where
 
 import qualified Data.Map                      as Map
 import           SSM.Core.Ident
 
 -- | Datatype that describes which GPIO pins are used.
 data GPIOPeripheral = GPIOPeripheral
-    { {- | This map associates a pin number with the name the reference is given in the
+  { {- | This map associates a pin number with the name the reference is given in the
       source code. -}
-      switchpins' :: Map.Map Int Ident
-    }
-    deriving (Show, Read, Eq)
+    switchpins' :: Map.Map Int Ident
+  }
+  deriving (Show, Read, Eq)
 
 {- | Create an initial GPIO Peripheral description. In the initial description, no GPIO
 pins are used. -}
@@ -38,12 +38,12 @@ emptyGPIOPeripheral = GPIOPeripheral Map.empty
 -- | Add a switch to a `GPIOPeripheral` and get the new peripheral back.
 addSwitchGPIO :: Int -> Ident -> GPIOPeripheral -> GPIOPeripheral
 addSwitchGPIO i id p = case Map.lookup i (switchpins' p) of
-    Just _ -> error $ concat
-        [ "SSM.Core.Peripheral.GPIO error: attempt to add switch "
-        , show i
-        , " but that switch has already been initialized"
-        ]
-    Nothing -> p { switchpins' = Map.insert i id (switchpins' p) }
+  Just _ -> error $ concat
+    [ "SSM.Core.Peripheral.GPIO error: attempt to add switch "
+    , show i
+    , " but that switch has already been initialized"
+    ]
+  Nothing -> p { switchpins' = Map.insert i id (switchpins' p) }
 
 -- | Get the switch GPIO pins from a `GPIOPeripheral`
 switchpins :: GPIOPeripheral -> [(Int, Ident)]
