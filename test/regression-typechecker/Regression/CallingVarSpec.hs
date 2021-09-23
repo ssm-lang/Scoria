124eaf8edd1b1e03723b5809e032fb2106e040b7
Yalu Cai <yc468@duke.edu>
2021-09-22 20:25:39 -0400

    three more test cases

---
+++ test/regression-typechecker/Regression/CallingVarSpec.hs
@@ -0,0 +1,41 @@
+module Regression.CallingVarSpec where
+
+import           Data.Map                       ( fromList )
+import SSM.Core.Syntax
+    ( Ident(Ident),
+      Procedure(Procedure, body, arguments, name),
+      Program(..),
+      Reference(Dynamic),
+      SSMExp(Lit, Var),
+      SSMLit(LUInt64, LBool, LInt32),
+      SSMTime(SSMTime),
+      SSMTimeUnit(SSMNanosecond),
+      Stm(After, NewRef, SetLocal, Fork),
+      Type(Ref, TUInt64, TBool, TInt32) )
+import qualified Test.Hspec                    as H
+import qualified Test.Hspec.QuickCheck         as H
+import qualified Test.SSM.Prop                 as T
+
+import SSM.Compile
+
+p :: Program
+p = Program
+  { entry = Ident "fun0" Nothing
+  , funs  = fromList
+              [ ( Ident "fun0" Nothing
+                , Procedure
+                  { name = Ident "fun0" Nothing
+                  , arguments = []
+                  , body = [ NewRef (Ident "ref0" Nothing) TBool (Lit TBool (LBool True))
+                           , SetLocal (Ident "var0" Nothing) TInt32 (Lit TInt32 (LInt32 42))
+                           , SetLocal (Ident "var1" Nothing) TInt32 (Var TInt32 (Ident "var0" Nothing))
+                           , Fork [ (Ident "var0" Nothing, [])]
+                           ]
+                  }
+                )
+              ]
+  , globalReferences = []
+  }
+
+spec :: H.Spec
+spec = T.typeIncorrectSpec "CallingVar" p
